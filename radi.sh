bb6bebb20ee2fb8b6ce48540dc3bb8772853ab16
m2ufb4xa <hmradiojpn@gmail.com>
2026-06-10 06:53:05 +0900

    Add user agent and headers to ffmpeg command
    リスラジ対応

--- radi.sh
+++ radi.sh
@@ -403,6 +403,8 @@ else
       -loglevel error \
       -fflags +discardcorrupt \
       -http_persistent 0 \
+      -user_agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36" \
+      -headers "Origin: http://listenradio.jp" \
       -i "${playlist_uri}" \
       -acodec copy \
       -vn \
